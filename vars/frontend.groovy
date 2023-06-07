54c4141278632549e0f5c43174cb207594520976
mana3abbas <112072190+mana3abbas@users.noreply.github.com>
2023-06-07 22:49:10 +0300

    Delete frontend.groovy
    efrewf

--- vars/frontend.groovy
+++
@@ -1,7 +0,0 @@
-def call (){
-    sh """
-                            docker login -u $USERNAME -p $PASSWORD
-                            docker build -t monasamir/client:l${BUILD_NUMBER} -f $WORKSPACE/badreads-frontend/
-                            docker push monasamir/client:l${BUILD_NUMBER}
-                          """
-}
