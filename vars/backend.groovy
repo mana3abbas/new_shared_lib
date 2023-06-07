feccd2aea5a90064eba608919c6a034f5bca44db
mana3abbas <112072190+mana3abbas@users.noreply.github.com>
2023-06-07 22:48:45 +0300

    Update and rename backend.groovy to build.groovyjbb

--- vars/backend.groovy
+++
@@ -1,7 +0,0 @@
-def call (){
-                    sh """
-                            docker login -u $USERNAME -p $PASSWORD
-                            docker build -t monasamir/server:s${BUILD_NUMBER} -f $WORKSPACE/badreads-backend/
-                            docker push monasamir/server:s${BUILD_NUMBER} 
-                       """
-}
---
+++ vars/build.groovy
@@ -0,0 +1,8 @@
+def call(String param1, String param2){
+                    sh """
+                            docker login -u $USERNAME -p $PASSWORD
+                            docker build -t monasamir${param1}:s${BUILD_NUMBER} -f $WORKSPACE/badreads-${param2}/
+                            docker push monasamir/${param1}:s${BUILD_NUMBER} 
+                       """
+}
+
