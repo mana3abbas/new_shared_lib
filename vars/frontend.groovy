769131d9611513a4742f6daf1d426f59a23b2898
mana3abbas <112072190+mana3abbas@users.noreply.github.com>
2023-06-07 15:48:24 +0300

    Update frontend.groovy

--- vars/frontend.groovy
+++ vars/frontend.groovy
@@ -1,7 +1,6 @@
 def call (){
     sh """
                             docker login -u $USERNAME -p $PASSWORD
-                            git branch: 'frontend', url: 'https://github.com/mana3abbas/new_shared_lib/tree/backend'
                             docker build -t monasamir/client:l${BUILD_NUMBER} -f $WORKSPACE/badreads-frontend/ 
                             docker push monasamir/client:l${BUILD_NUMBER}
                           """
