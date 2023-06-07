3a4e051009155d108167dc73c4998a4153b814c7
mana3abbas <112072190+mana3abbas@users.noreply.github.com>
2023-06-07 15:38:55 +0300

    Update backend.groovy

--- vars/backend.groovy
+++ vars/backend.groovy
@@ -1,8 +1,7 @@
 def call (){
                     sh """
                             docker login -u $USERNAME -p $PASSWORD
-                         
-                            docker build -t monasamir/server:b${BUILD_NUMBER} -f $WORKSPACE/badreads-backend/
-                            docker push monasamir/server:b${BUILD_NUMBER} 
+                            docker build -t monasamir/server:s${BUILD_NUMBER} -f $WORKSPACE/badreads-backend/
+                            docker push monasamir/server:s${BUILD_NUMBER} 
                        """
 }
