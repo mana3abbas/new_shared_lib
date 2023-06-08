cbe0a9f85a148d101e8727a83b2328a1232b136d
mana3abbas <112072190+mana3abbas@users.noreply.github.com>
2023-06-08 14:34:03 +0300

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,7 +1,7 @@
 def call(String param1, String param2){
                     sh """
                             docker login -u $USERNAME -p $PASSWORD
-                            docker build -t monasamir"${param1}":s${BUILD_NUMBER} -f $WORKSPACE/badreads-"${param2}"/ .
+                            docker build -t monasamir"${param1}":s${BUILD_NUMBER} -f $WORKSPACE/badreads-"${param2}"/
                             docker push monasamir/"${param1}":s${BUILD_NUMBER} 
                        """
 }
