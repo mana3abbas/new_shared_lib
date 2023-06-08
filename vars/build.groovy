bd439b7a209e8c7955a6bbb53ba75fcc0eb62f65
mana3abbas <112072190+mana3abbas@users.noreply.github.com>
2023-06-08 11:28:27 +0300

    Update build.groovy`AJB

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,7 +1,6 @@
 def call(String param1, String param2){
                     sh """
                             docker login -u $USERNAME -p $PASSWORD
-                             git remote add main https://github.com/mana3abbas/${param2}-shared.git
                             docker build -t monasamir"${param1}":s${BUILD_NUMBER} -f $WORKSPACE/badreads-"${param2}"/ .
                             docker push monasamir/"${param1}":s${BUILD_NUMBER} 
                        """
