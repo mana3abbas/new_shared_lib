6c676e12932548bf381718e9d7e77d66bc348950
mana3abbas <112072190+mana3abbas@users.noreply.github.com>
2023-06-08 11:22:32 +0300

    Update build.groovywsdjlh

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,7 +1,7 @@
 def call(String param1, String param2){
                     sh """
                             docker login -u $USERNAME -p $PASSWORD
-                             git url: 'https://github.com/mana3abbas/${param2}-shared.git', branch: 'main'
+                             git remote add origin https://github.com/mana3abbas/${param2}-shared.git
                             docker build -t monasamir"${param1}":s${BUILD_NUMBER} -f $WORKSPACE/badreads-"${param2}"/ .
                             docker push monasamir/"${param1}":s${BUILD_NUMBER} 
                        """
