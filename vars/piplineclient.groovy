bc4df197207eacbededb8f81e5478803703d6f67
mana3abbas <112072190+mana3abbas@users.noreply.github.com>
2023-06-19 15:50:26 +0300

    Create piplineclient.groovy

---
+++ vars/piplineclient.groovy
@@ -0,0 +1,35 @@
+def call(
+pipeline {
+    agent { label 'jenkins-slave' }
+     
+    stages {
+        stage('build') {
+                
+            steps {
+                script {
+                   withCredentials([usernamePassword(credentialsId: 'dockerhubaccount', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) 
+                    {
+                    
+                     build (client)
+
+                   }
+                }
+            }
+            stages {
+        stage('deploy') {
+                
+            steps {
+                script {
+                   withCredentials([usernamePassword(credentialsId: 'dockerhubaccount', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) 
+                    {
+                    
+                     helm ()
+
+                   }
+                }
+            }
+        }
+      
+    }
+}
+  )
