0f6782afeccef4770bc3a9b1ca40d0583f326911
mana3abbas <112072190+mana3abbas@users.noreply.github.com>
2023-06-19 15:46:07 +0300

    Update build.groovyasdfc

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,7 +1,7 @@
 def call(String param1){
                     sh """
                             docker login -u $USERNAME -p $PASSWORD
-                            docker build -t monasamir"${param1}":s${BUILD_NUMBER} -f  .
-                            docker push monasamir/"${param1}":s${BUILD_NUMBER} 
+                            docker build -t monasamir"${param1}":m${BUILD_NUMBER} -f  .
+                            docker push monasamir/"${param1}":m${BUILD_NUMBER} 
                        """
 }
