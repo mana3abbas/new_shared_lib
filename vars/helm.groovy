4330e9835f5e075ef6236733af31dc6cc927afa3
mana3abbas <112072190+mana3abbas@users.noreply.github.com>
2023-06-20 11:41:31 +0300

    Update helm.groovyljkg

--- vars/helm.groovy
+++ vars/helm.groovy
@@ -1,7 +1,14 @@
 def call(){
+          if
           
                 sh """
-                         echo "Running Helm"
-                         helm install vois${BUILD_NUMBER} ./chart
-                   """   
+                   echo "Running Helm"
+                   helm install web${BUILD_NUMBER} ./resources/chart  --values ./resources/chart/client.yaml
+                   """  
+          if 
+
+                 sh """
+                   echo "Running Helm"
+                   helm install web${BUILD_NUMBER} ./resources/chart  --values ./resources/chart/server.yaml
+                   """  
 }
