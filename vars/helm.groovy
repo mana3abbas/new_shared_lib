8ec1c2b2fe1e63deec3b0c89accbc6621237c530
mana3abbas <112072190+mana3abbas@users.noreply.github.com>
2023-06-20 11:19:57 +0300

    Update helm.groovyxzzc

--- vars/helm.groovy
+++ vars/helm.groovy
@@ -1,6 +1,5 @@
 def call(){
-                sh 'cd HELM'  
-                sh 'pwd'
+          
                 sh """
                          echo "Running Helm"
                          helm install vois${BUILD_NUMBER} ./chart
