1a20fc4e2409765ddcd423b30eabdfb5584294a0
mana3abbas <112072190+mana3abbas@users.noreply.github.com>
2023-06-20 11:45:50 +0300

    Update helm.groovy

--- vars/helm.groovy
+++ vars/helm.groovy
@@ -1,14 +1,9 @@
-def call(){
-          if
+def call(String param1){
+          
           
                 sh """
                    echo "Running Helm"
-                   helm install client${BUILD_NUMBER} ./resources/chart  --values ./resources/chart/client.yaml
+                   helm install ${param1}${BUILD_NUMBER} ./resources/chart  --values ./resources/chart/${param1}.yaml
                    """  
-          if 
-
-                 sh """
-                   echo "Running Helm"
-                   helm install server${BUILD_NUMBER} ./resources/chart  --values ./resources/chart/server.yaml
-                   """  
+           
 }
